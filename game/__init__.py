10c17a68023ad5c572b2f23d3de2446fca8e75d2
312-13\b1ky0164 <990201yiy@naver.com>
2024-12-26 10:06:55 +0900

    파이썬 패키지 연습(클래스 모듈 __init__)

---
+++ game/__init__.py
@@ -0,0 +1,13 @@
+from .graphic.render import render_test
+
+version = 3.11
+
+def print_version_info():
+    print(f"The version of this game is {version}")
+    print("게임 제작자 : 윤인영")
+    print("게임 제작자 이메일 : b1ky0164@naver.com")
+    print("게임 제작의뢰 : 010-1234-5678")
+
+#패키지 실행 초기화 문구
+print("Initializing game...")
+print("Loading.................")
